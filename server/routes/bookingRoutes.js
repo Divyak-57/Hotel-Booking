99ab2c03f2d686e8d9d02356f34d832aa03b3cab
Divya Kumari <divyakumaridk05@gmail.com>
2026-08-10 13:06:53 +0530

    Create bookingRoutes.js

---
+++ server/routes/bookingRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+import {
+    createBooking,
+    getMyBookings
+} from "../controllers/bookingController.js";
+
+const router = express.Router();
+
+router.post("/", createBooking);
+
+router.get("/my", getMyBookings);
+
+export default router;
