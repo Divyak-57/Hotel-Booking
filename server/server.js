48c8d14e60e38206adc02add8298d5c8b1ad6b91
Divya Kumari <divyakumaridk05@gmail.com>
2026-08-10 13:08:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
+
+import connectDB from "./config/db.js";
+
+import roomRoutes from "./routes/roomRoutes.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+connectDB();
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
+
 app.use(express.json());
+
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
     res.json({
@@ -15,6 +29,11 @@ app.get("/", (req, res) => {
     });
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+app.use("/api/rooms", roomRoutes);
+app.use("/api/bookings", bookingRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
